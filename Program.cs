71c3635b13f3bbc29f6c06ad348be5edcda181ff
AalexanderStudent <61276693+AalexanderStudent@users.noreply.github.com>
2021-10-16 15:04:51 +0300

    Lab 2 unfinished

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+
+
+        public static void ReplaceOperation(string Operation, Label labelOnly)
+        {
+            labelOnly.Text = Operation;
+        }
+
+        public static void ReplaceOperation(char Operation, Label labelOnly)
+        {
+            labelOnly.Text = Operation.ToString();
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            // exception
+            return a / b;
+        }
+
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
